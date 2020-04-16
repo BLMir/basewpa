73e2bcc648f1c99c428e88241a5063533d4a6eab
biel <biel.mir@trivago.com>
2020-04-16 18:42:05 +0200

    cleaning code

--- src/main/kotlin/com/base/baseApp/repositories/RolRepository.kt
+++ src/main/kotlin/com/base/baseApp/repositories/RolRepository.kt
@@ -3,5 +3,4 @@ package com.base.baseApp.repositories
 import com.base.baseApp.models.Rol
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface RolRepository: JpaRepository<Rol, Int> {
-}
+interface RolRepository: JpaRepository<Rol, Int>
